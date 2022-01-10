6a727d5a63db44e9936a71d6b27c7ebc45d4b035
Abhinav Manocha <abhinavmanocha@trentu.ca>
2022-01-09 20:53:02 -0500

    Centered Text inside of box with box shadow

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,32 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("This is my title"),
         ),
-        body: Container(
-          child: const Text("Hi flutter again"),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            width: 100,
+            height: 100,
+            clipBehavior: Clip.antiAlias,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              //shape: BoxShape.circle
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.blue,
+                  blurRadius: 300,
+                )
+              ]
+            ),
+            child:const Text("This is a ceneterd text inside of a div", 
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+          ),
         ),
     );
   }
